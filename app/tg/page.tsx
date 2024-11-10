a7032464258a041b94c5432717674440cd395169
lucifer0x17 <ayush.aa347@gmail.com>
2024-11-10 09:27:59 +0700

    feat: ai assitant added

--- app/tg/page.tsx
+++ app/tg/page.tsx
@@ -2,11 +2,18 @@
 import React, { useEffect, useState } from 'react'
 import { DynamicWidget, useAuthenticateConnectedUser, useDynamicContext } from "../../lib/dynamic";
 import Spinner from '@/components/Spinner';
+import { createTransaction } from '@/utils/brian';
 
 const Page = () => {
     const [isLoading, setIsLoading] = useState<boolean>(true);
     // const { sdkHasLoaded, user, } = useDynamicContext()
     const { isAuthenticating } = useAuthenticateConnectedUser()
+
+    const handleBrain = async () => {
+        const data = await createTransaction("Can you please transfer 100 USDC to megabyte0x.eth on Bnb", "0xa35CA8C065841dee7853C18bD9036B5A74f5988A")
+
+        console.log(data)
+    }
 
     // useEffect(() => {
     //     if (!sdkHasLoaded) return
@@ -17,6 +24,7 @@ const Page = () => {
     return (
         <>
             {isAuthenticating ? <Spinner /> : <DynamicWidget />}
+            <button onClick={handleBrain}>Test Brian</button>
         </>
     )
 }
